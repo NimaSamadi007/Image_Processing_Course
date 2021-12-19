05610b9bfb7f90925040261a250e247e5b7054b8
Nima Samadi <nbr.smd@gmail.com>
2021-12-19 23:11:37 +0330

    q3 completed - results added

--- HW3/q3.py
+++ HW3/q3.py
@@ -4,9 +4,10 @@ import utils as utl
 
 
 #------------------------ FUNCTIONS -------------------------- #
-def removeHolesFromResult(match_result, hole_indices):
+def removeHolesFromResult(match_result, hole_indices, extra_indices):
     "instead of holes in matching result, fills with 'inf' value to avoid selecting them"
     "hole_indices: dictionary of holes, each in the format of [left, right, top, bottom]"
+    "extra_indices: other places that I ignore to enhance performance"
     M, N = match_result.shape
     for i in range(len(hole_indices)):
         if hole_indices[i][0] - M_p >= 0:
@@ -29,20 +30,14 @@ def removeHolesFromResult(match_result, hole_indices):
         else:
             y2 = 0
         match_result[x1:x2, y1:y2] = np.inf
-    
+    for i in range(len(extra_indices)):
+        if extra_indices[i][0] != -1:
+            match_result[extra_indices[i][0]:extra_indices[i][1],
+                         extra_indices[i][2]:extra_indices[i][3]] = np.inf    
 
-def updateTexture(syn_tex, tex, M_p, N_p, M_i, N_i, x_s, x_f, y_s, y_f, x_thr, y_thr, rand_sel, hole_indices):
+def updateTexture(syn_tex, tex, M_p, N_p, x_s, y_s, x_thr, y_thr, 
+                  rand_sel, hole_indices, extra_indices):
     "updates texture at each step"
-    "syn_text: synthesized texture to be updated at each step"
-    "tex: original texture"
-    "(M_p, N_p): patches shape"
-    "M_i, N_i: synthesized image size"
-    "hole_indices: hole borders"
-    "x_s & x_f: start and final x indices"
-    "y_s & y_f: start and final y indices"
-    "x_thr & y_thr: x and y threshold used in extracting patches"
-    "rand_sel: number of random choices for choosing patch at each step"
-
     patch = np.copy(syn_tex[x_s:x_s+M_p, y_s:y_s+N_p, :])
 
     mask_template = np.ones(patch.shape, dtype=np.uint8)
@@ -51,7 +46,7 @@ def updateTexture(syn_tex, tex, M_p, N_p, M_i, N_i, x_s, x_f, y_s, y_f, x_thr, y
     matching_result = cv2.matchTemplate(tex, patch, cv2.TM_SQDIFF_NORMED, mask=mask_template)
     
     # remove results of holes:
-    removeHolesFromResult(matching_result, hole_indices)
+    removeHolesFromResult(matching_result, hole_indices, extra_indices)
 
     matching_indices = np.unravel_index(np.argsort(matching_result, axis=None)[0:rand_sel], 
                                                     matching_result.shape)
@@ -83,11 +78,6 @@ def updateTexture(syn_tex, tex, M_p, N_p, M_i, N_i, x_s, x_f, y_s, y_f, x_thr, y
     for i in range(M_p):
         mat_path_y[i, path_y[i, 0]] = 1
 
-    #repr = np.zeros((M_p, N_p, 3), dtype=int)
-    #repr[:, :, 0] = mat_path_x
-    #repr[:, :, 2] = mat_path_y
-    #repr *= 255
-    
     # mat_final showes boundries of patches
     tmp_mat = mat_path_x[0:x_thr, 0:y_thr] & mat_path_y[0:x_thr, 0:y_thr]
     common_points = np.nonzero(tmp_mat == 1)
@@ -133,46 +123,56 @@ def updateTexture(syn_tex, tex, M_p, N_p, M_i, N_i, x_s, x_f, y_s, y_f, x_thr, y
         if not flag:
             syn_tex[x_s:x_s+M_p, y_s+j] = patch[:, j]
 
+def runQ3(img_name, holes, extra_indices, x_thr, y_thr, M_p, N_p, random_select):
+    "runs complete algorithm for each image"
+    img = cv2.imread(img_name, cv2.IMREAD_COLOR)
+    print("Running algorithm for {}".format(img_name))
+    for k in range(len(holes)):
+    # hole to be filled
+        img_hole = np.copy(img[holes[k][0]-M_p:holes[k][1]+M_p, holes[k][2]-N_p: holes[k][3]+N_p, :])
+        M_i, N_i, _ = img_hole.shape
+        print("Filling hole {}".format(k))
+        for i in range(1, (M_i - M_p) // (M_p - x_thr) + 1):
+            for j in range(1, (N_i - N_p) // (N_p - y_thr) + 1):
+                x_end = i*M_p-(i-1)*(x_thr)
+                x_start = x_end - x_thr
+                y_end = j*N_p-(j-1)*(y_thr)
+                y_start = y_end - y_thr
 
+                updateTexture(img_hole, img, M_p, N_p,
+                            x_start, y_start, x_thr, y_thr, 
+                            random_select, holes, extra_indices)
 
-   
+        img[holes[k][0]-M_p:holes[k][1]+M_p, holes[k][2]-N_p:holes[k][3]+N_p, :] = img_hole
+
+    # filter to reduce borders
+    img = cv2.GaussianBlur(img, (3, 3), 1, borderType=cv2.BORDER_CONSTANT)
+
+    return img
 # ------------------------ MAIN ------------------------------- #
-img = cv2.imread("im03-hole.jpg", cv2.IMREAD_COLOR)
 
 ################### parameters #########################
-# hole borders dictionary in the [x_left, x_right, y_top, y_bottom] format
-#im04 holes border:
-#holes = {0:[684, 1170, 728, 970]}
+# hole borders dictionary in the [x_top, x_bottom, y_left, y_right] format
 
-#im03 holes border:
 holes = {0:[54, 167, 327, 543], 1:[614, 710, 1135, 1252], 2:[743, 931, 824, 976]}
-# overlapping threshold
+#extra places to ignore chosing from them
+extra_indices = {0:[0, 300, 650, 1400]}
 x_thr = 10
 y_thr = 10
-# patch size 
-M_p, N_p = 30, 30
+M_p, N_p = 40, 40
+random_select = 1
 
-random_select = 10
+img_filled = runQ3('im03-hole.jpg', holes, extra_indices, x_thr, y_thr, M_p, N_p, random_select)
+cv2.imwrite('res15.jpg', img_filled)
 
+#im04 holes border:
+holes = {0:[684, 1170, 728, 970]}
+# consider all possible choices
+extra_indices = {0:[-1, -1, -1, -1]}
+x_thr = 25
+y_thr = 25
+M_p, N_p = 100, 100
+random_select = 5
 
-for k in range(len(holes)):
-    # hole to be filled
-    img_hole = img[holes[k][0]-M_p:holes[k][1]+M_p, holes[k][2]-N_p: holes[k][3]+N_p, :]
-    #utl.showImg(img_hole, 1, 'hole')
-    M_i, N_i, _ = img_hole.shape
-    print("Filling hole {}".format(k))
-    for i in range(1, (M_i - M_p) // (M_p - x_thr) + 1):
-        for j in range(1, (N_i - N_p) // (N_p - y_thr) + 1):
-            x_end = i*M_p-(i-1)*(x_thr)
-            x_start = x_end - x_thr
-            y_end = j*N_p-(j-1)*(y_thr)
-            y_start = y_end - y_thr
-
-            updateTexture(img_hole, img, M_p, N_p,
-                          M_i, N_i, x_start, x_end, y_start, y_end,
-                          x_thr, y_thr, random_select, holes)
-            
-    img[holes[k][0]-M_p:holes[k][1]+M_p, holes[k][2]-N_p:holes[k][3]+N_p, :] = img_hole
-
-#utl.showImg(img_hole, 1)
-cv2.imwrite('im03-filled.jpg', img)
+img_filled = runQ3('im04-hole.jpg', holes, extra_indices, x_thr, y_thr, M_p, N_p, random_select)
+cv2.imwrite('res16.jpg', img_filled)
