fe22d86d13b4aecca96dcd194a1d1e4acc3ff42b
Nima <nbr.smd@gmail.com>
2022-01-07 19:40:15 +0330

    minor changes in q5

--- HW4/q5.py
+++ HW4/q5.py
@@ -10,8 +10,8 @@ def drawClosedContour(img, points, thic):
     N = points.shape[0]
     points_cv = np.stack([points[:, 1], points[:, 0]]).T
     for i in range(N):
-        cv2.line(img_cop, points_cv[i], points_cv[(i+1) % N], (0, 255, 0), thic)
-        # cv2.circle(img_cop, (points_cv[i, 0], points_cv[i, 1]), 5, (0, 255, 0), -1)
+        # cv2.line(img_cop, points_cv[i], points_cv[(i+1) % N], (0, 255, 0), thic)
+        cv2.circle(img_cop, (points_cv[i, 0], points_cv[i, 1]), 5, (0, 255, 0), -1)
     return img_cop
 
 def calAverageDistance(points):
@@ -21,7 +21,7 @@ def calAverageDistance(points):
     for i in range(N):
         d = np.sum((points - points[i])**2, axis=1, dtype=np.float64)
         d_avg += np.sum(np.sqrt(d))
-    return (d_avg / (N**2))
+    return (d_avg / ((N-1)**2))
 
 def calEnergy(img_grad, current_point, next_point, d_avg, alpha, gamma):
     "Calculates energy of E(v_i, v_i+1)"
@@ -43,43 +43,49 @@ def addMiddlePoints(points):
             else:
                 new_points[i, :] = (points[(i-1)//2] + points[(i+1)//2]) // 2
     return new_points
+
+def randomShifter(points):
+    "Shifts a sequence of points to the left"
+    N = points.shape[0]
+    index = np.random.randint(low=1, high=N)
+    # print(index)
+
+    shifted_points = np.zeros(points.shape, dtype=points.dtype)
+    shifted_points[0:N-index] = points[index:]
+    shifted_points[-index:] = points[:index]
+    
+    return shifted_points
 #---------------------------- MAIN ----------------------------------#
 # img = cv2.imread('Untitled.png', cv2.IMREAD_COLOR)
 img = cv2.imread('tasbih.jpg', cv2.IMREAD_COLOR)
 
 M_i, N_i, _ = img.shape
 
-# initial contours points
-# initial_points = np.array([[309, 221], [334, 198], [363, 176], [398, 169],
-#                             [423, 193], [435, 216], [465, 223], [503, 225], 
-#                             [526, 244], [515, 267], [505, 295], [510, 321],
-#                             [522, 353], [503, 381], [465, 386], [437, 401],
-#                             [415, 421], [380, 437], [344, 416], [320, 395],
-#                             [286, 380], [254, 373], [240, 341], [238, 310],
-#                             [236, 278], [241, 238], [274, 220]])
+# theta = np.linspace(0, 2*np.pi, 50)
+# v_x = np.int64(400 + 250*np.cos(theta))
+# v_y = np.int64(450 + 300*np.sin(theta))
 
-initial_points = np.array([[233, 261], [270, 242], [315, 230], [368, 223], 
-                           [405, 196], [445, 176], [494, 168], [524, 186], 
-                           [533, 222], [533, 254], [554, 282], [585, 299], 
-                           [630, 304], [676, 295], [730, 286], [783, 286], 
-                           [823, 294], [872, 300], [918, 320], [957, 341], 
-                           [925, 371], [875, 384], [824, 393], [769, 390], 
-                           [719, 392], [676, 394], [645, 403], [622, 426], 
-                           [603, 460], [575, 485], [534, 511], [481, 520], 
-                           [451, 507], [423, 489], [418, 511], [426, 550], 
-                           [420, 582], [390, 604], [347, 605], [309, 583], 
-                           [291, 547], [285, 506], [291, 470], [300, 437], 
-                           [309, 404], [314, 374], [277, 382], [216, 353], 
-                           [203, 322], [210, 289]])
+initial_points = np.array([[244, 210], [294, 199], [385, 181], [376, 163], 
+                            [436, 140], [503, 135], [555, 163], [581, 201], 
+                            [610, 243], [639, 281], [686, 294], [733, 318], 
+                            [737, 370], [699, 399], [674, 428], [646, 469], 
+                            [616, 507], [581, 540], [536, 566], [496, 540], 
+                            [457, 540], [451, 588], [417, 614], [369, 635], 
+                            [302, 636], [271, 590], [248, 525], [252, 469], 
+                            [250, 418], [199, 399], [163, 354], [166, 303], 
+                            [186, 257], [211, 225]])
 
-initial_points = addMiddlePoints(initial_points)
+# initial_points = np.stack([v_x, v_y]).T
+
+# initial_points = addMiddlePoints(initial_points)
+
 initial_points = np.stack([initial_points[:, 1], initial_points[:, 0]]).T
 
-#img_con = drawClosedContour(img, initial_points, 1)
-#utl.showImg(img_con, 0.5)
+# img_con = drawClosedContour(img, initial_points, 1)
+# utl.showImg(img_con, 0.5)
 
 # window size
-K = 5
+K = 3
 # number of states
 M = K**2
 # number of points on contour
@@ -94,18 +100,28 @@ table = np.zeros((M, M, N), dtype=np.float64)
 # shows predecessor of each state
 path = np.zeros((M, M, N), dtype=np.int64)
 
-alpha = 2
-gamma = 1
+
+alpha = 0.1
+gamma = 2
+# sigma = 3
 
 img_grad = utl.calImageGradient(img, 3, 'Scharr')
-#utl.showImg(img_grad, 0.5)
 
+# img_grad2 = utl.calImageGradient(img, 5, 'Scharr')
+
+# utl.showImg(utl.scaleIntensities(img_grad1, 'C'), 0.5, 't', False)
+# utl.showImg(utl.scaleIntensities(img_grad, 'C'), 0.5)
+# utl.showRange(img_grad)
 
 all_energy = np.zeros(M, dtype=np.float64)
 
-for step in range(50):
+max_step = 150
+for step in range(max_step):
+    # points = randomShifter(points)
     # average of length between points
-    d_avg = calAverageDistance(points)
+    # d_avg = calAverageDistance(points)
+    d_avg = 0
+    # print(d_avg)
     table[:, :, :] = 0
     path[:, :, :] = 0
     all_energy[:] = 0
@@ -151,20 +167,31 @@ for step in range(50):
     row_table = min_index // M
     best_state_index = min_index % M
     best_state = path[row_table, best_state_index, -1]
+    # previous_points = np.copy(points)
     for o in range(N-1, -1, -1):
         state_vector = np.array([best_state//K, best_state % K], dtype=np.int64)  
         points[o] += state_vector - (K-1)//2
         best_state = path[row_table, best_state, o]
-
     img_con = drawClosedContour(img, points, 1)
     cv2.imwrite('./test/res-{}.jpg'.format(step), img_con)
     print("going to step {}".format(step))
+    # print(np.sum(np.abs(previous_points - points)))
+    print("----------------------------")
+    # if not np.sum(np.abs(previous_points - points)) or step > max_step:
+    #     break
+    # else:
+    #     step += 1
 print("Done!")
+
 #%%
-print(points[0])
-for i in range(M):
-    row = i // K
-    col = i % K
-    print(i)
-    print(row-(K-1)//2, col-(K-1)//2)
-    print("----------------")
+x = points[0:10]
+
+y = randomShifter(x)
+
+img_con = drawClosedContour(img, x, 1)
+img_con2 = drawClosedContour(img, y, 1)
+utl.showImg(img_con, 0.5, 't', False)
+utl.showImg(img_con2, 0.5)
+
+
+
